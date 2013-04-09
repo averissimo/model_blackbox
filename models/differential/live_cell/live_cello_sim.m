3e14f827f94ee8295ff567b279bc6af73c0f747f
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2013-04-10 00:50:26 +0100

    corrects printing of header

--- models/differential/live_cell/live_cello_sim.m
+++ models/differential/live_cell/live_cello_sim.m
@@ -53,6 +53,6 @@ function [ string_output,output ] = live_cello_sim( test_data , draw_plot ) % <<
         msg = sprintf('{ "error": "%s" }\n',err.message);
         string_output = msg;
     end
-    fprintf(1,printHeader(0));
-    fprintf(1,string_output);
+    disp(printHeader(0));
+    disp(output_string);
 end
