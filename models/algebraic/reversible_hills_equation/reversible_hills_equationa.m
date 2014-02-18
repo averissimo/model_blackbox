77087e1bfc5443e241fc6c43d366cd26233a1b8d
Andre Verissimo <afsverissimo@gmail.com>
2014-02-18 19:05:08 +0000

    corrects test data

--- models/algebraic/reversible_hills_equation/reversible_hills_equationa.m
+++ models/algebraic/reversible_hills_equation/reversible_hills_equationa.m
@@ -15,7 +15,7 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-function [ F ] = reversible_hills_equationa( params,t )
+function [ F ] = reversible_hills_equationa( params,I )
 %reversible_hills_equationa Summary of this function goes here
 
 %% model parameters are extracted below
@@ -32,7 +32,7 @@ t  = params(10);
 vmax_PK  = params(11);
 
 %% function
-F = (vmax_PK .* 2/KmPEP .* 5/KmADP .* (1 - KeqPK .* (2 .* 0) ./ (5 .* 2))) .* (2 ./ KmPEP + 0 ./ KmPYR).^(n-1) .* (5 ./ KmADP + 2 ./ KmATP).^(n-1) ./ ( ((1 + (x./KI).^n + (2./KA).^n)./(1 + t.*(x./KI).^n + s * (2 ./ KA).^n) + (2 ./ KmPEP + 0 ./ KmPYR).^n) .* (((1 + (x./KI).^n + (2./KA).^n)./(1 + t.*(x./KI).^n + s * (2 ./ KA).^n)) + (5 ./ KmADP + 2 ./ KmATP).^n) );
+F = F = (vmax_PK .* 2/KmPEP .* 5/KmADP .* (1 - KeqPK .* (2 .* 0) ./ (5 .* 2))) .* (2 ./ KmPEP + 0 ./ KmPYR).^(n-1) .* (5 ./ KmADP + 2 ./ KmATP).^(n-1) ./ ( ((1 + (I./KI).^n + (2./KA).^n)./(1 + t.*(I./KI).^n + s * (2 ./ KA).^n) + (2 ./ KmPEP + 0 ./ KmPYR).^n) .* (((1 + (I./KI).^n + (2./KA).^n)./(1 + t.*(I./KI).^n + s * (2 ./ KA).^n)) + (5 ./ KmADP + 2 ./ KmATP).^n) );
 if size(F,1) > 1 F = F'; end
 
 end
