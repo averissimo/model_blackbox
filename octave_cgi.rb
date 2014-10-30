3ae72d3bccd4bf96492e62af5fc3560b487a6498
André Veríssimo <afsverissimo@gmail.com>
2014-10-30 16:36:04 +0000

    adds \n to end of new lines

--- octave_cgi.rb
+++ octave_cgi.rb
@@ -60,11 +60,11 @@ suffixes.each do |s|
         fid.write "addpath(genpath(\"#{File.expand_path("toolbox")}\"));\n"
         fid.write "addpath(genpath(\"#{File.expand_path("models")}\"));\n"
         # in case of post methods get the data directly from stdin(0)
-        fid.write "method = getenv('REQUEST_METHOD');"
-        fid.write "if strcmp(method,'POST') && length(getenv('QUERY_STRING') > 0"
-        fid.write " [val,count] = fread( 0 );"
-        fid.write " setenv('QUERY_STRING', sprintf('%c',val));"
-        fid.write "end"
+        fid.write "method = getenv('REQUEST_METHOD');\n"
+        fid.write "if strcmp(method,'POST') && length(getenv('QUERY_STRING') > 0\n"
+        fid.write " [val,count] = fread( 0 );\n"
+        fid.write " setenv('QUERY_STRING', sprintf('%c',val));\n"
+        fid.write "end\n"
         #
         fid.write "[output,string] = #{File.basename(f).sub(".m","")};\n"
 	fid.write "disp(printHeader( 0 ));\n"
