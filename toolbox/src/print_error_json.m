43daa9bf0aebfb098079deb947fc05747940cf6e
André Veríssimo <afsverissimo@gmail.com>
2014-10-30 18:22:30 +0000

    corrects error json

--- toolbox/src/print_error_json.m
+++ toolbox/src/print_error_json.m
@@ -17,15 +17,15 @@
 function string_output=print_error_json( err , fid, remove_bracket )
 %PRINT_ERROR_JSON Summary of this function goes here
 %   Detailed explanation goes here
-    string_output = "";
+    string_output = '';
     if nargin <= 2 || not(remove_bracket)
         string_output = strcat(string_output,sprintf('{\n'));
     end
-    string_output = strcat(string_output,sprintf('\t"error":\t"%s"\n',err.message));
+    string_output = strcat(string_output,sprintf('\t"error":\t"%s",\n',err.message));
     len = length(err.stack);
     for i = 1:len
         string_output = strcat(string_output,sprintf('',i));
-        string_output = strcat(string_output,sprintf('\t"stack_%d":\t[line: %d",\t"name":"%s",\tfile:"%s"]\n', i, err.stack(i).line, err.stack(i).name  , err.stack(i).file));
+        string_output = strcat(string_output,sprintf('\t"stack_%d":\t[line: %d",\t"name":"%s",\tfile:"%s"],\n', i, err.stack(i).line, err.stack(i).name  , err.stack(i).file));
     end
     string_output = strcat(string_output,sprintf('}\n'));
     fprintf(fid,string_output);
