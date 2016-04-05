31ac86b96269aea2581f4d0216db5bd1e561c9fb
André Veríssimo <afsverissimo@gmail.com>
2016-04-05 16:18:06 +0100

    working version

--- toolbox/src/get_inputs.m
+++ toolbox/src/get_inputs.m
@@ -8,50 +8,50 @@ function [ output_args ] = get_inputs( nargs, test_data, type, name )
     %
     %% get inputs
     % input paramters are in the environment variable "QUERY_STRING"
+    s = '';
+    ffid = fopen('/tmp/2veri_test.txt','a');
     if nargs > 0 && exist( 'test_data', 'var' )
+	fwrite(ffid, 'nargs > 0');
+    	fwrite(ffid, 10);
         if test_data == 1
+	  fwrite(ffid, '1');
+    	  fwrite(ffid, 10);
           s = test_query( type, name);
         elseif test_data == 0
-          %% check if it is a POST or GET method
-          method = getenv('REQUEST_METHOD');
-          if length(getenv('QUERY_STRING')) <= 0 && strcmp(method,'POST')
-              post = '';
-              fid = fopen('/dev/fd/0');
-              eof = 1;
-              while eof == 1
-                  post_tmp = fgets(fid,3000);
-                  if post_tmp == -1
-                      eof = 0;
-                  else
-                      post = strcat(post,post_tmp);
-                  end
-              end
-              s = post;
-              fclose(fid);
-          else
-              s = getenv('QUERY_STRING');
-          end
-        else
-          s = test_data;
-        end
-        output_args = qs2struct(s);
+	  fwrite(ffid, '2');
+    	  fwrite(ffid, 10);
+	  s = getenv('QUERY_STRING');
+	else
+          fwrite(ffid, '5');
+          fwrite(ffid, 10);
+	  s = test_data;
+	end
+	output_args = qs2struct(s);
     else
-        output_args = qs2struct(getenv('QUERY_STRING'));
+	fwrite(ffid, '6');
+        fwrite(ffid, 10);
+        %% check if it is a POST or GET method
+        method = getenv('REQUEST_METHOD');
+        if length(getenv('QUERY_STRING')) <= 0 && strcmp(method,'POST')
+      	  fwrite(ffid, '3');
+          fwrite(ffid, 10);
+          post = '';
+          fid = fopen('/dev/fd/0');
+          eof = 1;
+          while eof == 1
+              post_tmp = fgets(fid,3000);
+              if post_tmp == -1
+                  eof = 0;
+              else
+                  post = strcat(post,post_tmp);
+              end
+          end
+          fclose(fid);
+	  output_args = qs2struct(post);
+        else
+          output_args = qs2struct(getenv('QUERY_STRING'));
+        end
     end
 
     output_args = escape_uri( output_args );
-
-    ffid = fopen('/tmp/veri_test.txt','a');
-    fwrite(ffid, '-------------------------');
-    fwrite(ffid, 10);
-    fwrite(ffid, evalc(['disp(output_args)']));
-    fwrite(ffid, 10);
-    fwrite(ffid, output_args.querystring);
-    fwrite(ffid, 10);
-    fwrite(ffid, output_args.time);
-    fwrite(ffid, 10);
-    fwrite(ffid, output_args.values);
-    fwrite(ffid, 10);
-    fclose(ffid);
-
 end
